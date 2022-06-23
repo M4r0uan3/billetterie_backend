b38e2768429f4eee0fa8ae0c911d9f2ff62d956a
marwane33 <marwane.berhili@gmail.com>
2022-06-23 18:55:26 +0100

    update db settings.

--- billetterie_v2/settings/prod.py
+++ billetterie_v2/settings/prod.py
@@ -5,10 +5,9 @@ from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['ma-billetterie-prod.herokuapp.com']
+ALLOWED_HOSTS = ['mabilletterie-prod.herokuapp.com']
 
 SECRET_KEY = os.environ['SECRET_KEY']
-
 
 DATABASES = {
     'default': {
