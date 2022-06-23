3e21b7d77072e5f3bf945bc877a15c25d447a156
marwane33 <marwane.berhili@gmail.com>
2022-06-23 19:47:30 +0100

    updating prod.py

--- billetterie_v2/settings/dev.py
+++ billetterie_v2/settings/dev.py
@@ -13,3 +13,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- billetterie_v2/settings/prod.py
+++ billetterie_v2/settings/prod.py
@@ -11,3 +11,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
